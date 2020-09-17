707ab689e6bf4274387c44f7519bb39bfc7f15e7
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-17 18:45:37 +0000

    # for comments

--- command.c
+++ command.c
@@ -26,6 +26,8 @@ int command(char *buffer, stack_t **stack, unsigned int line)
 	token_array[i] = NULL;
 	if (strcmp(token_array[0], "push") == 0)
 		aux = op_push(stack, token_array[1], line);
+	else if (strcmp(token_array[0], "#") == 0)
+		op_nop(stack, line);
 	else
 		get_op(token_array[0], stack, line);
 	return (EXIT_SUCCESS);
